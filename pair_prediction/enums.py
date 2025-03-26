0438a8bbf4815484a240c8be81887191f6e36201
eestevens7 <58446368+eestevens7@users.noreply.github.com>
2025-03-26 16:46:04 -0400

    Add files via upload

---
+++ pair_prediction/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class SchemeOneBN(Enum):
+    CARBON = 2
+    NITROGEN = 1
+
+
+class SchemeTwoBN(Enum):
+    NITROGEN6 = 4
+    NITROGEN3 = 1
+
+
+class SchemeOneBE(Enum):
+    CARBON = "C"
+    NITROGEN = "N"
